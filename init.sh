31145e3f0a2da3ef2e7654d3da8ddffdf13f86c8
honndaj <lizeqing0@gmail.com>
2024-02-15 18:21:30 +0800

    feat(init.sh): add initial script for linux

---
+++ init.sh
@@ -0,0 +1,28 @@
+# 用root权限运行。这导致ssh公钥在是root的，所以git clone需要加sudo
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo '# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse' > /etc/apt/sources.list
+apt update
+
+apt install -y vim openssh-server git net-tools
+
+user='honndaj'
+email='lizeqing0@gmail.com'
+
+git config --global user.name "$user"
+git config --global user.email "$email"
+
+ssh-keygen -t rsa -C "$email"
+
