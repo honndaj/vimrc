d0c6435855c712d85c4594859f7f0cde96becda0
honndaj <73337294+honndaj@users.noreply.github.com>
2024-02-20 12:19:34 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-# 用root权限运行。这导致ssh公钥在是root的，所以git clone需要加sudo
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-echo '# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+# 如果脚本用root权限运行。这导致ssh公钥在是root的，所以git clone需要加sudo
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo echo '# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
@@ -16,7 +16,7 @@ deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe m
 # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse' > /etc/apt/sources.list
 apt update
 
-apt install -y vim openssh-server git net-tools
+sudo apt install -y vim openssh-server git net-tools
 
 user='honndaj'
 email='lizeqing0@gmail.com'
